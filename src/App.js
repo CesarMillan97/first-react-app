5a7049c22a0dd2e15d0f041780de9bf4f2ec7fa0
Cesar Millan <ces_pain@hotmail.com>
2022-04-07 16:58:02 -0500

    useState to create variables

--- src/App.js
+++ src/App.js
@@ -3,18 +3,46 @@ import './App.css';
 
 
 function App() {
-  
-  var name = 'Mario'
+  const [ showEvents, setShowEvents ] = useState(true)
+  const [ events, setEvents ] = useState([
+    { title: "Mario's birthday bash", id: 1 },
+    { title: "Bowser's live stream", id: 2 },
+    { title: "Race on moo moo farm", id: 3 },
+  ])
 
-  const handleClick = () =>{
-    name = 'Luigi'
-    console.log(name);
+  console.log(showEvents);
+
+  const handleClick = (id) => {
+    setEvents((prevEvents) => {
+      return prevEvents.filter((event) => {
+        return event.id !== id
+      })
+    })
+    console.log(id);
   }
 
   return (
     <div className="App">
-      <h1>My name is {name}</h1>
-      <button onClick={handleClick}>Change name</button>
+      {!showEvents && (
+        <div>
+          <button onClick={() => setShowEvents(true)}>Show Events</button>
+        </div>
+      )}
+      {showEvents && (
+        <div>
+          <button onClick={() => setShowEvents(false)}>Hide Events</button>
+        </div>
+      )}
+
+
+
+      {showEvents && events.map((event, index) => (
+        <div key={event.id}>
+          <h2>{ index } - { event.title }</h2>
+          <button onClick={() => handleClick(event.id) }>delete event</button>
+        </div>
+      ))}
+
     </div>
   );
 }
