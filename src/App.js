512e33315f6dcfee2482bad57c0493aa95c497cc
Cesar Millan <ces_pain@hotmail.com>
2022-04-08 13:48:08 -0500

    Modal

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import Title from './components/Title';
+import Modal from './components/Modal';
 
 
 function App() {
+  const [ showModal, setShowModal ] =useState(true)
   const [ showEvents, setShowEvents ] = useState(true)
   const [ events, setEvents ] = useState([
     { title: "Mario's birthday bash", id: 1 },
@@ -21,27 +24,40 @@ function App() {
     console.log(id);
   }
 
+  const handleClose = () => {
+    setShowModal(false);
+  }
+
+  const subtitle="All the latest event in Marioland"
+
   return (
     <div className="App">
+      <Title title="Events in your area" subtitle={subtitle} />
+
       {!showEvents && (
         <div>
           <button onClick={() => setShowEvents(true)}>Show Events</button>
         </div>
       )}
+
       {showEvents && (
         <div>
           <button onClick={() => setShowEvents(false)}>Hide Events</button>
         </div>
       )}
 
-
-
       {showEvents && events.map((event, index) => (
-        <div key={event.id}>
+        <React.Fragment key={event.id}>
           <h2>{ index } - { event.title }</h2>
           <button onClick={() => handleClick(event.id) }>delete event</button>
-        </div>
+        </React.Fragment>
       ))}
+      {showModal && (
+        <Modal handleClose={handleClose}>
+         <h2>10% Off Cupon Code!!</h2>
+         <p>Use the code CESAR123 at the checkout</p>
+        </Modal>
+      )}
 
     </div>
   );
---
+++ src/components/Modal.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Modal.css'
+
+export default function Modal(props) {
+  return (
+    <div className="modal-backdrop">
+       <div className='modal'>
+         {props.children}
+         <button onClick={ props.handleClose }>close</button>
+       </div>
+    </div>
+  )
+}
